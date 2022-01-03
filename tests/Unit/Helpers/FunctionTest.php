49b8d7b6abd5303cb38ab96dadcedeb19cc3305b
fcno <fabiocassiano@jfes.jus.br>
2022-01-03 18:46:20 -0300

    Testar o helper getLogLevels

--- tests/Unit/Helpers/FunctionTest.php
+++ tests/Unit/Helpers/FunctionTest.php
@@ -4,6 +4,7 @@
  * @link https://pestphp.com/docs/
  */
 
+use function App\Helpers\getLogLevels;
 use function App\Helpers\stringToArrayAssoc;
 
 test('stringToArrayAssoc retorna null se valores inválidos forem informados', function($keys, $string, $delimitador) {
@@ -27,3 +28,19 @@ test('stringToArrayAssoc explode a string com base no delimitador e retorna um a
 
     expect(stringToArrayAssoc($keys, $string, $delimitador))->toMatchArray($esperado);
 });
+
+test('getLogLevels retorna os níveis de log nos termos da PSR-3', function() {
+
+    expect(getLogLevels()->toArray())->toMatchArray([
+        "DEBUG"     => 100,
+        "INFO"      => 200,
+        "NOTICE"    => 250,
+        "WARNING"   => 300,
+        "ERROR"     => 400,
+        "CRITICAL"  => 500,
+        "ALERT"     => 550,
+        "EMERGENCY" => 600
+    ]);
+});
+
+
