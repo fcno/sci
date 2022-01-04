23dfe9fb56a7bda76b3c8050a06d931b583ac819
fcno <fabiocassiano@jfes.jus.br>
2022-01-04 01:03:42 -0300

    Importar os arquivos de log de impressão do FS

---
+++ app/Importer/PrintLogImporter.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace App\Importer;
+
+use App\Events\ExceptionEvent;
+use App\Events\FailureEvent;
+use App\Events\RegularEvent;
+use App\Importer\PrintImporter;
+use Bcremer\LineReader\LineReader;
+use Illuminate\Support\{Arr, Str};
+use Illuminate\Support\Facades\{App, Storage};
+use Throwable;
+
+/**
+ * Importador dos arquivos de log de impressão.
+ */
+final class PrintLogImporter
+{
+    /**
+     * File System em que estão armazenados os logs de impressão
+     *
+     * @var \Illuminate\Contracts\Filesystem\Filesystem $file_system
+     */
+    private $file_system;
+
+    /**
+     * Create new class instance.
+     */
+    public function __construct()
+    {
+        $this->file_system = Storage::disk('log-impressao');
+    }
+
+    /**
+     * Create new class instance.
+     *
+     * @return static
+     */
+    public static function make(): static
+    {
+        return new static();
+    }
+
+    /**
+     * Importa os logs de impressão que estão no File System
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        $this->start();
+        $this->process();
+        $this->finish();
+    }
+
+    /**
+     * Tratativas iniciais para a importação.
+     *
+     * @return void
+     */
+    private function start(): void
+    {
+        RegularEvent::dispatch(
+            __('job.importacao.iniciada', ['attribute' => __('strings.log_impressao')]),
+            'notice',
+            null
+        );
+    }
+
+    /**
+     * Execução propriamente dita da importação.
+     *
+     * @return void
+     */
+    private function process(): void
+    {
+        foreach($this->printLogFiles() as $print_log_file) {
+
+            $saved = $this->save($print_log_file);
+
+            if ($saved) {
+                $this->delete($print_log_file);
+            }
+
+            RegularEvent::dispatch(
+                __('job.importacao.processado', ['attribute' => $print_log_file]),
+                'info',
+                null
+            );
+        }
+    }
+
+    /**
+     * Lista de arquivos de log de impressão para serem importados.
+     *
+     * Não retonra o full path, mas tão somente o file name, filtrando os
+     * arquivos para não retornar os logs de erro de impressão.
+     *
+     * @return array  Ex.: `['21-02-2020.txt', '22-02-2020.txt', ... ]`
+     */
+    private function printLogFiles(): array
+    {
+        return Arr::where(
+            $this->file_system->files(),
+
+            function (string $print_log_file) {
+                return !Str::of($print_log_file)->contains('erro');
+            }
+        );
+    }
+
+    /**
+     * Persistência para todas as impressões presentes no arquivo de log.
+     *
+     * @param string $print_log_file  Ex.: 21-02-2012.txt
+     *
+     * @return bool true se a persistência integral do arquivo foi feita ou
+     * false caso alguma linha do arquivo tenha falhado.
+     */
+    private function save(string $print_log_file): bool
+    {
+        /**
+         * Utiliza-se a biblioteca LineReader para fazer a leitura dos arquivos
+         * de log, pois eles podem ser grandes o que poderia levar ao estouro
+         * de memória.
+         * Assim, em vez de se ler o arquivo inteiro de uma vez, a biblioteca
+         * faz a leitura do arquivo linha por linha.
+         * Se necessário, para ver o consumo de mermória, basta colocar o
+         * trecho abaixo onde se deseja medi-lo.
+         *
+         * php echo memory_get_peak_usage(false)/1024/1024 . PHP_EOL;
+         *
+         * Para maiores informações:
+         * https://www.php.net/manual/pt_BR/ini.core.php#ini.memory-limit
+         * https://www.php.net/manual/pt_BR/function.memory-get-usage.php
+         * https://stackoverflow.com/questions/15745385/memory-get-peak-usage-with-real-usage
+         * https://www.sitepoint.com/performant-reading-big-files-php/
+         * https://github.com/bcremer/LineReader
+         */
+        try {
+
+            foreach (LineReader::readLines($this->fullPath($print_log_file)) as $print) {
+
+                PrintImporter::make()->run((string) $print);
+
+            }
+
+            return true;
+        } catch (Throwable $exception) {
+
+            ExceptionEvent::dispatch(
+                __('error.arquivo.importacao', ['attribute' => $print_log_file]),
+                $exception,
+                'critical',
+                [
+                    'file' => $print_log_file,
+                    'disk' => $this->file_system
+                ]
+            );
+
+            return false;
+        }
+    }
+
+    /**
+     * Caminho completo do arquivo informado
+     *
+     * @param string  $print_log_file  ex.: 21-02-2012.txt
+     *
+     * @return string Full path
+     */
+    private function fullPath(string $print_log_file): string
+    {
+        return $this->file_system->path($print_log_file);
+    }
+
+    /**
+     * Exclui o arquivo log de impressão informado.
+     *
+     * @param string  $print_log_file  ex.: 21-02-2012.txt
+     *
+     * @return void
+     */
+    private function delete(string $print_log_file): void
+    {
+        if(App::environment('production')) {
+
+            $deleted = $this->file_system->delete($print_log_file);
+
+            FailureEvent::dispatchIf(! $deleted,
+                __('error.arquivo.delete', ['attribute' => $print_log_file]),
+                'critical',
+                [
+                    'file' => $print_log_file,
+                    'disk' => $this->file_system
+                ]
+            );
+
+        } else {
+            RegularEvent::dispatch(
+                __('strings.del_not_producao', ['attribute' => $print_log_file]),
+                'info',
+                null
+            );
+        }
+    }
+
+    /**
+     * Conclusão do processamento de importação.
+     *
+     * @return void
+     */
+    private function finish(): void
+    {
+        RegularEvent::dispatch(
+            __('job.importacao.concluido', ['attribute' => __('strings.log_impressao')]),
+            'notice',
+            null
+        );
+    }
+}
---
+++ tests/Feature/Importer/PrintLogImporterTest.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * @link https://pestphp.com/docs/
+ */
+
+use App\Events\ExceptionEvent;
+use App\Events\FailureEvent;
+use App\Events\RegularEvent;
+use App\Importer\PrintLogImporter;
+use App\Models\{Cliente, Impressao, Impressora, Servidor, Usuario};
+use Illuminate\Support\Facades\{App, Event, Storage};
+
+beforeEach(function() {
+
+    $this->print_log_files = [
+        '01-12-2020.txt' =>
+            "server1.domain.gov.br╡01/12/2020╡08:00:00╡report.pdf╡jesaaa╡2021╡╡╡CPU-10000╡IMP-111╡1000╡4╡2" . PHP_EOL .
+            "server2.domain.gov.br╡01/12/2020╡10:30:00╡private.pdf╡jesbbb╡2021╡╡╡CPU-10000╡IMP-222╡5000╡8╡2" . PHP_EOL,
+
+        '02-12-2020.txt' =>
+            "server1.domain.gov.br╡02/12/2020╡11:00:00╡report.pdf╡jesaaa╡2021╡╡╡CPU-10000╡IMP-333╡3000╡4╡2" . PHP_EOL .
+            "server1.domain.gov.br╡02/12/2020╡13:15:15╡games.pdf╡jesccc╡2021╡╡╡CPU-20000╡IMP-222╡1000╡4╡1" . PHP_EOL .
+            "server2.domain.gov.br╡02/12/2020╡18:01:50╡rules.pdf╡jesccc╡2021╡╡╡CPU-20000╡IMP-111╡2000╡9╡2" . PHP_EOL,
+
+        '03-12-2020.txt' => ''
+    ];
+
+    $this->fake_disk = Storage::fake('log-impressao');
+
+    foreach ($this->print_log_files as $file_name => $content) {
+        $this->fake_disk->put($file_name, $content);
+    }
+});
+
+afterEach(function() {
+    $this->fake_disk = Storage::fake('log-impressao');
+});
+
+test('o importador retorna o objeto da classe corretamente no método make', function () {
+    expect(PrintLogImporter::make())->toBeInstanceOf(PrintLogImporter::class);
+});
+
+test('importa o log de impressão corretamente', function () {
+
+    PrintLogImporter::make()->run();
+
+    expect(Impressao::get())->toHaveCount(5)
+    ->and(Servidor::get())->toHaveCount(2)
+    ->and(Usuario::get())->toHaveCount(3)
+    ->and(Cliente::get())->toHaveCount(2)
+    ->and(Impressora::get())->toHaveCount(3);
+
+});
+
+test('dispara o evento RegularEvent para registrar o início, a conclusão, a importação do log e a sua exclusão caso estivesse em produção', function () {
+
+    Event::fake();
+
+    PrintLogImporter::make()->run();
+
+    //1 inicio, 1 conclusão e 3 tentativas de exclusão (uma para cada arquivo) e três processamentos de arquivo.
+    Event::assertDispatched(RegularEvent::class, 8);
+    Event::assertNotDispatched(FailureEvent::class);
+    Event::assertNotDispatched(ExceptionEvent::class);
+
+    //arquivos não foram excluídos após importação, pois não está em produção.
+    $this->fake_disk->assertExists(array_keys($this->print_log_files));
+
+    expect(Impressao::get())->toHaveCount(5);
+});
+
+test('exclui todos os arquivos importados se estiver em produção', function () {
+
+    $this->fake_disk->assertExists(array_keys($this->print_log_files));
+
+    App::shouldReceive('environment')
+        ->times(3) //Será acionado uma vez para a exclusão de cada arquivo
+        ->andReturn('production');
+
+    PrintLogImporter::make()->run();
+
+    $this->fake_disk->assertMissing(array_keys($this->print_log_files));
+
+    expect(Impressao::get())->toHaveCount(5);
+});
